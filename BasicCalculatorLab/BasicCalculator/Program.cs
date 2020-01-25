3711fe3c19fe6ab58006182a780e1a95ae000bb9
dschnoor2 <59981825+dschnoor2@users.noreply.github.com>
2020-01-25 12:18:57 -0600

    Update Program.cs
    Updates weather formulas

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -11,14 +11,21 @@ namespace BasicCalculator
             Console.WriteLine("Enter Temperature in Fahrenhiet");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int Temp = int.Parse(Console.ReadLine());
+            int tempFahr = int.Parse(Console.ReadLine()); //tempFahr = Temperature in Fahrenheit
 
-            Console.WriteLine("Enter Relative Humdidity");
-            int RH = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Relative Humidity");
+            int rH = int.Parse(Console.ReadLine());  //rH = Relative Humidity
 
-            decimal dewPoint = Temp -  9 *(100 - RH) / 25;
+            double dewPoint = tempFahr -  9 * (100 - rH) / 25;
 
-            Console.WriteLine("The answer is " + dewPoint);
+            Console.WriteLine("The Dew Point is " + dewPoint);
+
+            Console.WriteLine("Enter Wind Speed");
+            int windSpeed = int.Parse(Console.ReadLine());
+
+            double windChill = 35.74 + (0.6215 * tempFahr) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * tempFahr * Math.Pow(windSpeed, 0.16);
+
+            Console.WriteLine("The Wind Chill is " + windChill);
         }
     }
 }
